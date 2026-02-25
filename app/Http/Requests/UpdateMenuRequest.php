74e5d6764db5fe9b5b065bf9af2fecbb92e5478b
git2025 <thetthetmon.mgaway@gmail.com>
2026-02-25 14:49:35 +0630

    fix: category id edit

--- app/Http/Requests/UpdateMenuRequest.php
+++ app/Http/Requests/UpdateMenuRequest.php
@@ -19,7 +19,7 @@ class UpdateMenuRequest extends FormRequest
         return [
             'title' => ['sometimes','string','max:255'],
             'price' => ['sometimes','integer','min:0'],
-            'category_id' => ['sometimes', 'exists:categories,id'],
+            'category_id' => ['sometimes','nullable','exists:categories,id'],
             'image' => ['sometimes','file','image','max:2048'],
         ];
     }
